d67fb58f2789f70c7a789c9dabbc929d9e9f24a3
bassy <baberna2001@gmail.com>
2023-04-14 16:00:17 +0200

    front qui marche bien la

---
+++ front/webStatistique.js
@@ -0,0 +1,35 @@
+const btnServers = document.getElementById('btn-servers');
+const btnAges = document.getElementById('btn-ages');
+const stats = document.getElementById('stats');
+const loading = document.getElementById('loading');
+
+btnServers.addEventListener('click', () => {
+    // Afficher le message d'attente
+    loading.style.display = 'block';
+    stats.textContent = '';
+
+    // Envoyer la requête GET
+    fetch('http://localhost:9000/question1')
+        .then(console.log("reponse recup ok"))
+        .then(response => response.text())
+        .then(data => {
+            // Cacher le message d'attente et afficher la réponse
+            loading.style.display = 'none';
+            stats.textContent = data;
+        }).catch(error => console.error(error));
+});
+
+btnAges.addEventListener('click', () => {
+    // Afficher le message d'attente
+    loading.style.display = 'block';
+    stats.textContent = '';
+
+    // Envoyer la requête GET
+    fetch('http://localhost:9000/question2')
+        .then(response => response.text())
+        .then(data => {
+            // Cacher le message d'attente et afficher la réponse
+            loading.style.display = 'none';
+            stats.textContent = data;
+        }).catch(error => console.error(error));
+});
